b6d1329eedc5c312822e88b91f906ac2cc257ff2
Takahiro_Fujii <225126c@gs.kochi-tech.ac.jp>
2023-04-24 21:39:58 +0900

    タブレットはカード2枚並べに変更。カードから文字がはみ出る場合は「...」を表示するように変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,12 +34,12 @@ export default function Home({ allPostsData }) {
             <section class="body-font text-gray-600">
                 <div class="container mx-auto px-1 pb-24 md:pt-4">
                     <div class="flex flex-wrap">
-                        <div class="p-4 md:w-1/3">
+                        <div class="p-4 md:w-1/2 lg:w-1/3">
                             <Link href="/posts/sample">
                                 <div class="overflow-hidden rounded-lg border-2 border-gray-200 border-opacity-60 duration-300 hover:scale-105">
                                     <img class="w-full object-cover object-center md:h-36 lg:h-48" src="images/thumbnail01.jpg" alt="blog" />
                                     <div class="p-6">
-                                        <h1 class="title-font mb-3 border-l-neutral-700 text-lg font-medium">Next.jsでブログサイトを作る</h1>
+                                        <h1 class="title-font mb-3 truncate border-l-neutral-700 text-lg font-medium">Next.jsでブログサイトを作る</h1>
                                         <div class="flex flex-wrap items-center">
                                             <p class="inline-flex items-center  border-l-neutral-700 md:mb-2 lg:mb-0">更新日 : 2023/4/23</p>
                                         </div>
@@ -47,12 +47,12 @@ export default function Home({ allPostsData }) {
                                 </div>
                             </Link>
                         </div>
-                        <div class="p-4 md:w-1/3">
+                        <div class="p-4 md:w-1/2 lg:w-1/3">
                             <Link href="/posts/sample">
                                 <div class="overflow-hidden rounded-lg border-2 border-gray-200 border-opacity-60 duration-300 hover:scale-105">
                                     <img class="w-full object-cover object-center md:h-36 lg:h-48" src="images/thumbnail01.jpg" alt="blog" />
                                     <div class="p-6">
-                                        <h1 class="title-font mb-3 border-l-neutral-700 text-lg  font-medium">The 400 Blows</h1>
+                                        <h1 class="title-font mb-3 truncate border-l-neutral-700 text-lg  font-medium">The 400 Blows</h1>
                                         <div class="flex flex-wrap items-center">
                                             <p class="inline-flex items-center  border-l-neutral-700 md:mb-2 lg:mb-0">Learn More</p>
                                         </div>
@@ -60,12 +60,12 @@ export default function Home({ allPostsData }) {
                                 </div>
                             </Link>
                         </div>
-                        <div class="p-4 md:w-1/3">
+                        <div class="p-4 md:w-1/2 lg:w-1/3">
                             <Link href="/posts/sample">
                                 <div class="overflow-hidden rounded-lg border-2 border-gray-200 border-opacity-60 duration-300 hover:scale-105">
                                     <img class="w-full object-cover object-center md:h-36 lg:h-48" src="images/thumbnail01.jpg" alt="blog" />
                                     <div class="p-6">
-                                        <h1 class="title-font mb-3 border-l-neutral-700 text-lg font-medium">The 400 Blows</h1>
+                                        <h1 class="title-font mb-3 truncate border-l-neutral-700 text-lg font-medium">The 400 Blows</h1>
                                         <div class="flex flex-wrap items-center">
                                             <p class="inline-flex items-center  border-l-neutral-700 md:mb-2 lg:mb-0">Learn More</p>
                                         </div>
@@ -73,12 +73,12 @@ export default function Home({ allPostsData }) {
                                 </div>
                             </Link>
                         </div>
-                        <div class="p-4 md:w-1/3">
+                        <div class="p-4 md:w-1/2 lg:w-1/3">
                             <Link href="/posts/sample">
                                 <div class="overflow-hidden rounded-lg border-2 border-gray-200 border-opacity-60 duration-300 hover:scale-105">
                                     <img class="w-full object-cover object-center md:h-36 lg:h-48" src="images/thumbnail01.jpg" alt="blog" />
                                     <div class="p-6">
-                                        <h1 class="title-font mb-3 border-l-neutral-700 text-lg  font-medium">The 400 Blows</h1>
+                                        <h1 class="title-font mb-3 truncate border-l-neutral-700 text-lg  font-medium">The 400 Blows</h1>
                                         <div class="flex flex-wrap items-center">
                                             <p class="inline-flex items-center  border-l-neutral-700 md:mb-2 lg:mb-0">Learn More</p>
                                         </div>
