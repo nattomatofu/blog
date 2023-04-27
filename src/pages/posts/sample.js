4e2e12e8b15227663f52b8f24e0c60c46f9cd7a9
Takahiro_Fujii <225126c@gs.kochi-tech.ac.jp>
2023-04-27 22:49:17 +0900

    記事を追記

--- src/pages/posts/sample.js
+++ src/pages/posts/sample.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export const title = "Next.jsでブログを作ってみた（※技術的な内容はありません）";
 export const updateDate = "2023/4/25";
 
@@ -57,31 +59,31 @@ const sample = () => {
                 <div className="mt-8">
                     <p className="md:p-8">このブログ作成には以下の技術やプログラミング言語を使っています。</p>
                     <div className="md:pl-8">
-                        <h3 className="my-2 font-bold">HTML</h3>
+                        <h3 className="my-2 font-bold underline decoration-dotted underline-offset-4">HTML</h3>
                         <p className="pl-4">
                             おそらく皆さんご存じのHTML(HyperText Markup Language)です。
                             <br />
                             Webサイトを作るうえで基本中の基本の言語ということで使用しています。
                         </p>
-                        <h3 className="my-2 font-bold">CSS(Tailwindcss)</h3>
+                        <h3 className="my-2 font-bold underline decoration-dotted underline-offset-4">CSS(Tailwindcss)</h3>
                         <p className="pl-4">
                             HTMLで作成したコンテンツをデザインするための言語で「Cascading Style Sheets」の略だそうです。
                             <br />
                             今回はプレーンのCSSを使用するか、Bootstrapなどのフレームワークを使用するかで少し悩みましたが、 CSSの基本を理解したいという気持ちの上に楽もしたいという気持ちがあったので、
                             中間くらいに位置するTailwindcssを使用することにしました。
                         </p>
-                        <h3 className="my-2 font-bold">JavaScript</h3>
+                        <h3 className="my-2 font-bold underline decoration-dotted underline-offset-4">JavaScript</h3>
                         <p className="pl-4">
                             HTML, CSSときたらJavaScriptですよね。フロントエンドではサイトのコンテンツに動きを付ける時などに使われます。
                             今回は静的なブログを作成するだけなのでJavaScriptはなくても作れるのですが、 以前から下記のReactというJavaScriptのフレームワークに興味があったので使ってみました。
                         </p>
-                        <h3 className="my-2 font-bold">React</h3>
+                        <h3 className="my-2 font-bold underline decoration-dotted underline-offset-4">React</h3>
                         <p className="pl-4">
                             JavaScriptのフレームワークで、コンテンツをコンポーネントという単位でまとめて管理することができます。
                             <br />
                             ブログ程度の静的なサイトだとHTMLとCSSのみでも問題なく作ることができるのですが、前述の通りReactというものに以前から興味があったので、ページ自体や良く使うHTMLタグをコンポーネント化するために使用しました。
                         </p>
-                        <h3 className="my-2 font-bold">Next.js</h3>
+                        <h3 className="my-2 font-bold underline decoration-dotted underline-offset-4">Next.js</h3>
                         <p className="pl-4">
                             JavaScriptのフレームワークであるReactのさらにフレームワークであるNext.jsです。
                             <br />
@@ -91,14 +93,14 @@ const sample = () => {
                             <br />
                             Next.jsはReactについて調べている間に存在を知り、Reactを使うなら便利そうだ！使ってみよう！と思ったのがきっかけです。（Next.jsのSSG, SSRの詳しい仕組みは現在勉強中です。。）
                         </p>
-                        <h3 className="my-2 font-bold">Vercel</h3>
+                        <h3 className="my-2 font-bold underline decoration-dotted underline-offset-4">Vercel</h3>
                         <p className="pl-4">
                             作成したサイト（今回だとブログ）を世に配信するためのサービスです。 Next.jsを開発している会社がVercelという名前で、社名と同名のサービスとなります。
-                            Next.jsの開発会社が開発したサービスということもあってNext.jsとの相性は抜群だそうで、
+                            Next.jsの開発会社が開発しているということもあってNext.jsとの相性は抜群だそうで、
                             Gitの更新をトリガーにしてビルド→デプロイの流れを自動で行ってくれます。（こちらも現在勉強中です。。）
                         </p>
                     </div>
-                    <p className="md:p-8">
+                    <p className="pt-8 md:p-8">
                         本ブログで使った技術はざっと以上となります。
                         <br />
                         続いて、以上のプログラミング言語や技術をどのように勉強したかを書こうと思うのですが、その前にブログ制作を始める前の私のスキルについて簡単に触れようと思います。
@@ -134,7 +136,7 @@ const sample = () => {
                     <p className="md:p-8">上記の通り、フロントエンドについてのスキルはほぼ皆無な私ですが、その状態からどんな過程で学習してこの記事を執筆するまでに至ったか、期間別でご紹介します。</p>
                 </div>
                 <div className="md:pl-8">
-                    <h3 className="my-2 font-bold">0～1ヶ月半程度 : HTML, CSSの勉強</h3>
+                    <h3 className="my-2 font-bold underline decoration-dotted underline-offset-4">0～1ヶ月半程度 : HTML, CSSの勉強</h3>
                     <p className="pl-4">
                         始めの1か月半の学習はほぼ全てHTMLとCSSの習得にあてていました。
                         <br />
@@ -145,7 +147,7 @@ const sample = () => {
                         <br />
                         ちなみにHTML, CSSに限らず、基本的に勉強はUdemyというオンライン学習サイトを使って勉強しています。
                     </p>
-                    <h3 className="my-2 font-bold">1ヶ月半～2ヶ月 : JavaScriptの勉強</h3>
+                    <h3 className="my-2 font-bold underline decoration-dotted underline-offset-4">1ヶ月半～2ヶ月 : JavaScriptの勉強</h3>
                     <p className="pl-4">
                         大体1ヶ月半～2ヶ月の半月ではJavaScriptを主に学習していました。
                         <br />
@@ -154,7 +156,7 @@ const sample = () => {
                         ただ、せっかく勉強するならある程度深い部分まで知っておきたいという思いがあったので、
                         同期処理/非同期処理の違いやProxyなどの少し難しめの内容も勉強していたこともあり、少し長めに時間を取りました。（現時点の当ブログではそれら技術は本当に全く使ってないんですけどね、、）
                     </p>
-                    <h3 className="my-2 font-bold">2ヶ月～2ヶ月半 : Reactの勉強</h3>
+                    <h3 className="my-2 font-bold underline decoration-dotted underline-offset-4">2ヶ月～2ヶ月半 : Reactの勉強</h3>
                     <p className="pl-4">
                         ここに来てやっとですが、Reactの勉強を始めます。 実をいうと、ブログを作ろうと思ってまず初めにUdemyでReactの講座を受講したのですが、HTMLやCSSが全く分からず、
                         Reactの内容が全く頭に入ってこなかったので、HTML, CSSの勉強時間をまず初めに多めに取り、このタイミングでReactの勉強を始めました。
@@ -163,7 +165,7 @@ const sample = () => {
                         <br />
                         ただ、コンポーネントを分ける単位や、コンポーネント間でデータを共有するuseContextやReduxをどの区切りで整理するのが良いのかは、実際にWeb制作を経験して学ぶ必要があるなーと思っています。
                     </p>
-                    <h3 className="my-2 font-bold">2ヶ月半～3ヶ月 : Next.jsの勉強とブログ制作</h3>
+                    <h3 className="my-2 font-bold underline decoration-dotted underline-offset-4">2ヶ月半～3ヶ月 : Next.jsの勉強とブログ制作</h3>
                     <p className="pl-4">
                         ここでいきなりブログを作り始めます笑。Reactの勉強を始めた頃から早くブログを作りたい欲が高まっていたんですよね笑。
                         <br />
@@ -174,7 +176,97 @@ const sample = () => {
                         Next.jsを使ったブログの制作から公開まで一気に学ぶことが出来る講座がUdemyにあったからというのが早く作れた一番の理由です。はい。）
                     </p>
                 </div>
-                <p className="md:p-8">ブログを作ろう！と思い立ってからこの記事を書くまでの経緯は以上のような感じでした。 せっかくなので私が受講したUdemyの講座をご紹介させていただきます。</p>
+                <p className="pt-8 md:p-8">
+                    ブログを作ろう！と思い立ってからこの記事を書くまでの経緯は以上のような感じでした。
+                    <br />
+                    <br />
+                    せっかくなので私が受講したUdemyの講座をご紹介させていただきます。全てとても分かりやすい講座でした！
+                </p>
+                <section class="body-font text-gray-600">
+                    <div class="container mx-auto flex flex-wrap px-5 py-6">
+                        <div class="-m-4 flex flex-wrap">
+                            <div class="p-4 md:w-full">
+                                <div class="flex flex-col rounded-lg border-2 border-gray-200 border-opacity-50 p-8 sm:flex-row">
+                                    <div class="flex-grow">
+                                        <h2 class="title-font mb-3 text-lg font-medium text-gray-900">HTML/CSSの講座</h2>
+                                        <ul className="list-disc">
+                                            <li>
+                                                <a href="https://www.udemy.com/course/html5css3-b/" className="cursor-pointer text-blue-500 underline decoration-blue-500 hover:scale-105">
+                                                    【最新2022】未経験からHTML、CSS をマスターして、WEBデザイナー・エンジニアを目指す　最高の実践コース
+                                                </a>
+                                            </li>
+                                            <li className="pt-2">
+                                                <a href="https://www.udemy.com/course/html-css-js/" className="cursor-pointer text-blue-500 underline decoration-blue-500 hover:scale-105">
+                                                    ちゃんと学ぶ、HTML/CSS + JavaScript
+                                                </a>
+                                                （JavaScriptも含まれていますが、HTML/CSSの印象が強かったのでHTML/CSSに入れさせていただきました）
+                                            </li>
+                                            <li className="pt-2">
+                                                <a href="https://www.udemy.com/course/tailwindcss_beginner/" className="cursor-pointer text-blue-500 underline decoration-blue-500 hover:scale-105">
+                                                    【tailwindcss】初心者向け講座・CSSが苦手な人向け 【わかりやすさ重視】【次世代のCSS】にじっくり取り組む
+                                                </a>
+                                            </li>
+                                        </ul>
+                                        {/* <p class="text-base leading-relaxed">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p> */}
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="p-4 md:w-full">
+                                <div class="flex flex-col rounded-lg border-2 border-gray-200 border-opacity-50 p-8 sm:flex-row">
+                                    <div class="flex-grow">
+                                        <h2 class="title-font mb-3 text-lg font-medium text-gray-900">JavaScriptの講座</h2>
+                                        <ul className="list-disc">
+                                            <li>
+                                                <a href="https://www.udemy.com/course/javascript-essence/" className="cursor-pointer text-blue-500 underline decoration-blue-500 hover:scale-105">
+                                                    【JS】ガチで学びたい人のためのJavaScriptメカニズム
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="p-4 md:w-full">
+                                <div class="flex flex-col rounded-lg border-2 border-gray-200 border-opacity-50 p-8 sm:flex-row">
+                                    <div class="flex-grow">
+                                        <h2 class="title-font mb-3 text-lg font-medium text-gray-900">Reactの講座</h2>
+                                        <ul className="list-disc">
+                                            <li>
+                                                <a href="https://www.udemy.com/course/react-complete-guide/" className="cursor-pointer text-blue-500 underline decoration-blue-500 hover:scale-105">
+                                                    【2023年最新】React(v18)完全入門ガイド｜Hooks、Next.js、Redux、TypeScript
+                                                </a>
+                                            </li>
+                                            <li className="pt-2">
+                                                <a
+                                                    href="https://www.udemy.com/course/modern_javascipt_react_beginner/"
+                                                    className="cursor-pointer text-blue-500 underline decoration-blue-500 hover:scale-105"
+                                                >
+                                                    モダンJavaScriptの基礎から始める挫折しないためのReact入門
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="p-4 md:w-full">
+                                <div class="flex flex-col rounded-lg border-2 border-gray-200 border-opacity-50 p-8 sm:flex-row">
+                                    <div class="flex-grow">
+                                        <h2 class="title-font mb-3 text-lg font-medium text-gray-900">Next.jsの講座</h2>
+                                        <ul className="list-disc">
+                                            <li>
+                                                <a
+                                                    href="https://www.udemy.com/course/nextjs-microblog-for-beginner/"
+                                                    className="cursor-pointer text-blue-500 underline decoration-blue-500 hover:scale-105"
+                                                >
+                                                    【Next.js入門】ReactフレームワークのNext.jsでマイクロブログを構築しながら基礎と本質を学ぶ講座
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </section>
             <section>
                 <div className="mt-12 border-b border-l-4 border-neutral-700 border-l-neutral-700">
