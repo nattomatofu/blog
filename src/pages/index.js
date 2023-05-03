189ceba9315e0813414822ed50cf5f15b720b793
Takahiro_Fujii <225126c@gs.kochi-tech.ac.jp>
2023-05-03 18:51:52 +0900

    tailwind line clampを追加。カードを最大横並び2つに変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import Link from "next/link";
-import Layout from "@/components/Layout";
 import { getPostsData } from "@/lib/post";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -35,12 +34,12 @@ export default function Home({ allPostsData }) {
             <section class="body-font text-gray-600">
                 <div class="container mx-auto px-1 md:pt-4 xl:px-48">
                     <div class="flex flex-wrap">
-                        <div class="w-full p-4 md:w-1/2 lg:w-1/3">
+                        <div class="w-full p-4 md:w-1/2">
                             <Link href="/posts/made-blog-with-nextjs">
                                 <div class="overflow-hidden rounded-lg border-2 border-gray-200 border-opacity-60 duration-300 hover:scale-105">
                                     <img class="w-full object-cover object-center md:h-36 lg:h-48" src="images/nextjs_thumbnail.jpg" alt="blog" />
                                     <div class="p-6">
-                                        <h1 class="title-font mb-3 truncate border-l-neutral-700 text-lg font-medium">Next.jsでブログを作ってみた（※技術的な内容はありません）</h1>
+                                        <h1 class="title-font mb-3 border-l-neutral-700 text-lg font-medium line-clamp-1">Next.jsでブログを作ってみた（※技術的な内容はありません）</h1>
                                         <div class="flex flex-wrap items-center">
                                             <p class="inline-flex items-center  border-l-neutral-700 md:mb-2 lg:mb-0">更新日 : 2023/4/29</p>
                                         </div>
