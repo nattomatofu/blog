ba9efb23ebe37a053941c7e26d700adca1723208
t-fujii <taka1211312@gmail.com>
2023-09-25 22:54:41 +0900

    Pythonの記事を追記

--- src/pages/posts/python-memo.js
+++ src/pages/posts/python-memo.js
@@ -13,12 +13,13 @@ import CodeText from "@/components/CodeText";
 import CodeBlockTitle from "@/components/CodeBlockTitle";
 import CodeBlock from "@/components/CodeBlock";
 import Head from "next/head";
+import UnderLineBoldText from "@/components/UnderLineBoldText";
 
 const title = "Pythonについての個人的なメモ";
-const updateDate = "2023/9/24";
+const updateDate = "2023/9/25";
 const thumbnailImagePath = "/images/thumbnail/python_logo.webp";
 const metaDescription =
-    "どうも、納豆大好きnattoです。仕事でよくPythonを使うので、最近は改めてPythonを勉強し直しています。その中で個人的に勉強になったことを備忘録としてまとめます。";
+    "どうも、納豆大好きnattoです。仕事でよくPythonを使うので、最近は改めてPythonを勉強し直しています。その中で個人的に勉強になったことやカンペとして残しておきたいことをまとめます。";
 const metaOgUrl = "https://www.nattomatofu.com/posts/python-memo";
 const metaOgType = "article";
 
@@ -52,9 +53,9 @@ const PythonMemo = () => {
                             <PrefaceText>
                                 仕事でよくPythonを使うので、最近改めてPythonを勉強し直しています。
                                 <br />
-                                その中で個人的に勉強になったことを備忘録としてまとめます。
+                                その中で個人的に勉強になったことや、カンペとして残しておきたいことをまとめます。
                                 <br />
-                                カンペとして活用したいので、めちゃくちゃ初歩的な内容も多いです笑
+                                めちゃくちゃ初歩的な内容も多いです笑
                             </PrefaceText>
                             <TableOfContents />
                         </Preface>
@@ -71,6 +72,10 @@ const PythonMemo = () => {
                                 </CodeBlock>
                                 このように入力することで、input()の中に記載した文字が表示された後にユーザーからの入力を受け付けます。
                                 <br />
+                                <UnderLineBoldText>
+                                    戻り値の方はStringです。
+                                </UnderLineBoldText>
+                                <br />
                                 ちなみに、print関数と以下のような組み合わせ方も可能です。
                                 <CodeBlock language={"python"} isLineNum={true}>
                                     {`print("こんにちは、" + input("名前を入力してください"))`}
@@ -82,6 +87,35 @@ const PythonMemo = () => {
                                 以下のように使用することで引数に渡した文字列の長さを取得できます。
                                 <CodeBlock language={"python"} isLineNum={true}>
                                     {`len(任意の文字列)`}
+                                </CodeBlock>
+                            </MediumParagraph>
+                            <MediumHeading>type関数</MediumHeading>
+                            <MediumParagraph>
+                                以下のように使用することで引数に渡した変数の方を確認できます。
+                                <CodeBlock language={"python"} isLineNum={true}>
+                                    {`type(変数)`}
+                                </CodeBlock>
+                            </MediumParagraph>
+                            <MediumHeading>int関数, round関数</MediumHeading>
+                            <MediumParagraph>
+                                引数に渡した変数をint型へ変換します。
+                                <br />
+                                <CodeBlock language={"python"} isLineNum={true}>
+                                    {`int(変数)`}
+                                </CodeBlock>
+                                <UnderLineBoldText>
+                                    このint関数でfloatからintへ変換した場合、小数点以下は全て切り捨てられます。
+                                    <br />
+                                    四捨五入したい場合は以下のようにround関数を使用しましょう。
+                                </UnderLineBoldText>
+                                <CodeBlock language={"python"} isLineNum={true}>
+                                    {`round(変数)`}
+                                </CodeBlock>
+                                また、round関数では小数点何桁まで値を保持したいかを、以下のように引数で指定することができます。
+                                <CodeBlock language={"python"} isLineNum={true}>
+                                    {`float_num = 1.2345
+int_num = round(float_num, 2) # →　二桁目まで残す
+print(int_num) # →　1.23と表示される`}
                                 </CodeBlock>
                             </MediumParagraph>
                         </MainParagraph>
@@ -97,6 +131,35 @@ const PythonMemo = () => {
                                 <CodeBlock language={"python"} isLineNum={true}>
                                     {`a = 123456789
 b = 123_456_789 # Pythonが「_」は無視してくれる`}
+                                </CodeBlock>
+                            </MediumParagraph>
+                            <MediumHeading>
+                                文字列の特定のインデックス番目の文字を取得
+                            </MediumHeading>
+                            <MediumParagraph>
+                                以下のように書くことで、文字列の特定のインデックス番目の文字を取得することができます。
+                                <CodeBlock language={"python"} isLineNum={true}>
+                                    {`str = "Hello"
+str[1] # 一番目の文字を取得　→　H
+"Hello"[1]  # 同じく一番目の文字を取得　→　H`}
+                                </CodeBlock>
+                            </MediumParagraph>
+                            <MediumHeading>指数の計算</MediumHeading>
+                            <MediumParagraph>
+                                Pythonでは以下のような書き方で指数の計算を行うことができます。
+                                <CodeBlock language={"python"} isLineNum={true}>
+                                    {`result = 2 ** 3
+print(result) # 2の3乗で「8」が表示される`}
+                                </CodeBlock>
+                            </MediumParagraph>
+                            <MediumHeading>
+                                小数点以下を切り落として除算
+                            </MediumHeading>
+                            <MediumParagraph>
+                                以下のように書くことで、小数点以下を切り落として除算することができます。
+                                <CodeBlock language={"python"} isLineNum={true}>
+                                    {`result = 8 // 3 # 普通に割ると 2.666666666....
+print(result) # でも2になる`}
                                 </CodeBlock>
                             </MediumParagraph>
                             <MediumHeading>変数の入れ替え</MediumHeading>
