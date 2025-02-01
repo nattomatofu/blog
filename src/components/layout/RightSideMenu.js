f06987ac37b549b4f9ac0a772efe63f22fe16fb8
t-fujii <taka1211312@gmail.com>
2025-02-01 16:59:00 +0900

    サイドメニューに広告を追加

--- src/components/layout/RightSideMenu.js
+++ src/components/layout/RightSideMenu.js
@@ -7,6 +7,21 @@ const RightSideMenu = () => {
             <div className="container mx-auto mt-8 space-y-4 p-6 text-gray-600 lg:mx-0 lg:mt-0 lg:w-4/12 xl:pr-52">
                 <ProfileCard />
                 <ArchivedMenuForEachMonth />
+                <div
+                    dangerouslySetInnerHTML={{
+                        __html: `
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          <ins class="adsbygoogle"
+               style="display:block"
+               data-ad-client="ca-pub-XXXXXXXXXXXXXXXX"
+               data-ad-slot="YYYYYYYYYY"
+               data-ad-format="auto"></ins>
+          <script>
+               (adsbygoogle = window.adsbygoogle || []).push({});
+          </script>
+        `,
+                    }}
+                />
             </div>
         </>
     );
